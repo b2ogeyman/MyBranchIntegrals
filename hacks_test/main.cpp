eedeb516937a3042f29d6ed8d9b82289834c8c3b
b2ogeyman <kostyamyagkov@gmail.com>
2017-04-01 17:12:12 -0700

    Initial Commit

---
+++ hacks_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hacks_test
+//
+//  Created by kostya on 4/1/17.
+//  Copyright © 2017 kostya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
